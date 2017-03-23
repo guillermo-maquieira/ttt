2e828c7c0c573660e52debaf237b33c7681b3534
Guillermo Maquieira <guimaq@gmail.com>
2017-03-23 00:26:50 -0300

    Add unit tests created using PHP Storm

---
+++ tests/BoardTest.php
@@ -0,0 +1,12 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: guillermo
+ * Date: 23/03/17
+ * Time: 00:09
+ */
+class BoardTest extends PHPUnit_Framework_TestCase
+{
+
+}
---
+++ tests/UsersTest.php
@@ -0,0 +1,12 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: guillermo
+ * Date: 23/03/17
+ * Time: 00:10
+ */
+class UsersTest extends PHPUnit_Framework_TestCase
+{
+
+}
